6dcc55d4229f0e9963eed55f3317559d95f6eed4
hithagovi <hithgovi@gmail.com>
2025-07-22 12:23:35 +0530

    Initial commit

---
+++ fash/script1.js
@@ -0,0 +1,10 @@
+// Example interactivity
+document.getElementById('write-blog').addEventListener('click', () => {
+    alert('Redirecting to Write a Blog page...');
+    // Logic for redirection can be added here
+});
+
+document.getElementById('logout').addEventListener('click', () => {
+    alert('You have logged out!');
+    // Logic for logout can be added here
+});
